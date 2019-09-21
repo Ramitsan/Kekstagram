9bd796b68e93586550683464e938cf34c88824ca
Ramitsan <ramitsan@gmail.com>
2019-09-21 15:44:15 +0300

    Массивы

---
+++ js/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var URL_PHOTOS = ['photos/001.jpg', 'photos/002.jpg', 'photos/003.jpg', 'photos/004.jpg', 'photos/005.jpg', 'photos/006.jpg', 'photos/007.jpg', 'photos/008.jpg', 'photos/009.jpg', 'photos/010.jpg', 'photos/011.jpg', 'photos/012.jpg', 'photos/013.jpg', 'photos/014.jpg', 'photos/015.jpg', 'photos/016.jpg', 'photos/017.jpg', 'photos/018.jpg', 'photos/019.jpg', 'photos/020.jpg', 'photos/021.jpg', 'photos/022.jpg', 'photos/023.jpg', 'photos/024.jpg', 'photos/025.jpg',];
+var DESCRIPTION_PHOTOS = [];
+var LIKES_PHOTOS = [];
+var COMMENTS_PHOTOS = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
+var NAMES_AUTORS_PHOTOS = ['Артем', 'Кекс', 'Профи', 'Рудольф', 'Кира А.', 'Михаил В.', 'Анна', 'Alexandro778853', 'Тамара61', 'Elvis'];
