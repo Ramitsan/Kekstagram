7c6eb0f203766926a8a8ca10e77601c342f1b627
Ramitsan <ramitsan@gmail.com>
2019-09-22 14:25:07 +0300

    Создание массивов фотографий и комментов. Начало

--- js/main.js
+++ js/main.js
@@ -1,13 +1,79 @@
 'use strict';
 
-var URL_PHOTOS = ['photos/001.jpg', 'photos/002.jpg', 'photos/003.jpg', 'photos/004.jpg', 'photos/005.jpg', 'photos/006.jpg', 'photos/007.jpg', 'photos/008.jpg', 'photos/009.jpg', 'photos/010.jpg', 'photos/011.jpg', 'photos/012.jpg', 'photos/013.jpg', 'photos/014.jpg', 'photos/015.jpg', 'photos/016.jpg', 'photos/017.jpg', 'photos/018.jpg', 'photos/019.jpg', 'photos/020.jpg', 'photos/021.jpg', 'photos/022.jpg', 'photos/023.jpg', 'photos/024.jpg', 'photos/025.jpg',];
-var DESCRIPTION_PHOTOS = [];
-var LIKES_PHOTOS = [];
+var DESCRIPTION_PHOTOS = ['Снято на Кэнон', 'Снято на Никон', 'Снято на телефон'];
 var COMMENTS_PHOTOS = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
 var NAMES_AUTORS_PHOTOS = ['Артем', 'Кекс', 'Профи', 'Рудольф', 'Кира А.', 'Михаил В.', 'Анна', 'Alexandro778853', 'Тамара61', 'Elvis'];
+var PHOTOS_AMOUNT = 25;
+var AVATAR_AMOUNT = 6;
+var MIN_LIKES = 15;
+var MAX_LIKES = 200;
+
+
+// генерация случайного числа в заданном интервале, включительно
+var getRandomIndex = function(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+// генерируем url фото
+var photoUrl = 'photos/' + getRandomIndex(1, PHOTOS_AMOUNT) + '.jpg';
+console.log(photoUrl);
+
+// генерируем случайное количество лайков
+var likes = getRandomIndex(MIN_LIKES, MAX_LIKES);
+console.log(likes);
+
+// генерируем url случайного аватара
+var numberAvatar = 'img/avatar-' + getRandomIndex(1, AVATAR_AMOUNT) + '.svg';
+console.log(numberAvatar);
+
+
+// функция генерации одного комментария
+var createPhotoComment = function(_avatar, _message, _name) {
+  var comment = {
+    avatar: numberAvatar,
+    message: getRandomIndex(COMMENTS_PHOTOS),
+    name: getRandomIndex(NAMES_AUTORS_PHOTOS)
+  }
+  return comment;
+};
+
+//функция генерации массива комментариев
+var createPhotoComments = function(length) {
+  var comments = [];
+  for (var i = 0; i <= length; i++) {
+    var indexComment = getRandomIndex(0, comments.length);
+    comments.push(createPhotoComment(comments.length[indexComment], ));
+  }
+  return comments;
+};
+
+
+// функция создания одного объекта с фото
+var createPhotoObject = function(_url, _description, _likes, _comments) {
+  var photo = {
+    url: photoUrl,
+    description: getRandomIndex(DESCRIPTION_PHOTOS),
+    likes: likes,
+    comment: getRandomIndex(comments)
+  }
+  return photo;
+};
+
+
+// функция создания массива объектов
+var createPhotoObjects = function(length) {
+  var photos = [];
+
+  for (var i = 0; i <= length; i++) {
+    var indexPhoto = getRandomIndex(0, photos.length);
+    photos.push(createPhotoObject(photos.length[indexPhoto], ));
+  }
+  return photos;
+};
