ff96299e76144b84ca0284896e249e19b38d74f9
Ramitsan <ramitsan@gmail.com>
2019-09-23 20:25:10 +0300

    Создание массивов фотографий и комментов. Продолжение

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ var COMMENTS_PHOTOS = [
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
 var NAMES_AUTORS_PHOTOS = ['Артем', 'Кекс', 'Профи', 'Рудольф', 'Кира А.', 'Михаил В.', 'Анна', 'Alexandro778853', 'Тамара61', 'Elvis'];
-var PHOTOS_AMOUNT = 25;
+var PHOTOS_AMOUNT = 24;
 var AVATAR_AMOUNT = 6;
 var MIN_LIKES = 15;
 var MAX_LIKES = 200;
@@ -18,28 +18,34 @@ var MAX_LIKES = 200;
 
 // генерация случайного числа в заданном интервале, включительно
 var getRandomIndex = function(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min)) + min;
 };
 
-// генерируем url фото
-var photoUrl = 'photos/' + getRandomIndex(1, PHOTOS_AMOUNT) + '.jpg';
-console.log(photoUrl);
+// генерируем случайный текст случайного коммента
+var getRandomMessage = function (arr) {
+  var randomMessage = getRandomIndex(0, arr.length);
+  return arr[randomMessage];
+};
 
-// генерируем случайное количество лайков
-var likes = getRandomIndex(MIN_LIKES, MAX_LIKES);
-console.log(likes);
+// генерируем случайное имя
+var getRandomName = function (arr) {
+  var randomName = getRandomIndex(0, arr.length);
+  return arr[randomName];
+};
 
-// генерируем url случайного аватара
-var numberAvatar = 'img/avatar-' + getRandomIndex(1, AVATAR_AMOUNT) + '.svg';
-console.log(numberAvatar);
+// генерируем случайное описание фото
+var getRandomDescription = function (arr) {
+  var randomDescription = getRandomIndex(0, arr.length);
+  return arr[randomDescription];
+};
 
 
 // функция генерации одного комментария
 var createPhotoComment = function(_avatar, _message, _name) {
   var comment = {
-    avatar: numberAvatar,
-    message: getRandomIndex(COMMENTS_PHOTOS),
-    name: getRandomIndex(NAMES_AUTORS_PHOTOS)
+    avatar: _avatar,
+    message: _message,
+    name: _name
   }
   return comment;
 };
@@ -48,20 +54,33 @@ var createPhotoComment = function(_avatar, _message, _name) {
 var createPhotoComments = function(length) {
   var comments = [];
   for (var i = 0; i <= length; i++) {
-    var indexComment = getRandomIndex(0, comments.length);
-    comments.push(createPhotoComment(comments.length[indexComment], ));
+    var numberAvatar = 'img/avatar-' + getRandomIndex(1, AVATAR_AMOUNT) + '.svg';
+    var message = getRandomMessage(COMMENTS_PHOTOS);
+    var name = getRandomName(NAMES_AUTORS_PHOTOS);
+
+    comments[i] = createPhotoComment(numberAvatar, message, name);
   }
   return comments;
+};
+
+var arrComments = createPhotoComments(PHOTOS_AMOUNT);
+console.log(arrComments);
+
+
+// функция генерации случайного комментария из массива комментариев
+var getRandomComment = function (arr) {
+  var randomComment = getRandomIndex(0, arr.length);
+  return arr[randomComment];
 };
 
 
 // функция создания одного объекта с фото
 var createPhotoObject = function(_url, _description, _likes, _comments) {
   var photo = {
-    url: photoUrl,
-    description: getRandomIndex(DESCRIPTION_PHOTOS),
-    likes: likes,
-    comment: getRandomIndex(comments)
+    url: _url,
+    description: _description,
+    likes: _likes,
+    comment: _comments
   }
   return photo;
 };
@@ -70,10 +89,17 @@ var createPhotoObject = function(_url, _description, _likes, _comments) {
 // функция создания массива объектов
 var createPhotoObjects = function(length) {
   var photos = [];
-
   for (var i = 0; i <= length; i++) {
-    var indexPhoto = getRandomIndex(0, photos.length);
-    photos.push(createPhotoObject(photos.length[indexPhoto], ));
+    var photoUrl = 'photos/' + getRandomIndex(1, PHOTOS_AMOUNT) + '.jpg';
+    var description = getRandomDescription(DESCRIPTION_PHOTOS);
+    var likes = getRandomIndex(MIN_LIKES, MAX_LIKES);
+    var comment = getRandomComment(arrComments);
+
+    photos[i] = createPhotoObject(photoUrl, description, likes, comment);
   }
   return photos;
 };
+
+
+var fff = createPhotoObjects(PHOTOS_AMOUNT);
+console.log(fff);
