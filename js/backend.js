5ab8ec0a2def70479534e37ce8ef0de0723b854d
Ramitsan <ramitsan@gmail.com>
2019-10-21 12:49:32 +0300

    Добавление функции создания сетевых запросов

---
+++ js/backend.js
@@ -0,0 +1,57 @@
+'use strict';
+
+(function () {
+  var URL_SAVE = 'https://js.dump.academy/kekstagram';
+  var URL_LOAD = 'https://js.dump.academy/kekstagram/data';
+  var REQUEST_TIMEOUT = 10000; // 10s
+  var REQUEST_STATUS_OK = 200;
+  var REQUEST_STATUS_BAD = 400;
+  var REQUEST_STATUS_NOT_FOUND = 404;
+  var REQUEST_STATUS_SERVER_ERROR = 500;
+
+
+  window.serverRequest = function (URL, method, data, onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    xhr.timeout = REQUEST_TIMEOUT;
+    xhr.open(method, URL);
+
+    xhr.addEventListener('load', function () {
+      switch (xhr.status) {
+        case REQUEST_STATUS_OK:
+          onLoad(xhr.response);
+          break;
+        case REQUEST_STATUS_BAD:
+          onError('Неправильный запрос.  Код ошибки ' + xhr.status);
+          break;
+        case REQUEST_STATUS_NOT_FOUND:
+          onError('Страница не найдена. Код ошибки ' + xhr.status);
+          break;
+        case REQUEST_STATUS_SERVER_ERROR:
+          onError('Внутренняя ошибка сервера. Код ошибки ' + xhr.status);
+          break;
+        default:
+          onError('При загрузке произошла ошибка ' + xhr.status + '. Повторите попытку позже.');
+      }
+    });
+
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.send(data);
+  };
+
+  window.save = function (data, onLoad, onError) {
+    window.serverRequest(URL_SAVE, 'POST', data, onLoad, onError);
+  };
+
+  window.load = function (onLoad, onError) {
+    window.serverRequest(URL_LOAD, 'GET', null, onLoad, onError);
+  };
+
+})();
